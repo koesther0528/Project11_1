8f5b464cd9369ddaad6dac185471d7b4ec67109e
esthe <ko990727@hmail.com>
2021-07-27 10:24:50 +0900

    Edit MainActivity.java Commit

--- app/src/main/java/kr/hs/emirim/ko502804/project11_1/GridAdapter.java
+++ app/src/main/java/kr/hs/emirim/ko502804/project11_1/GridAdapter.java
@@ -1,5 +1,6 @@
 package kr.hs.emirim.ko502804.project11_1;
 
+import android.app.AlertDialog;
 import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
@@ -40,6 +41,23 @@ public class GridAdapter extends BaseAdapter {
         imgv.setScaleType(ImageView.ScaleType.FIT_CENTER);
         imgv.setPadding(5,5,5,5);
         imgv.setImageResource(posterIds[i]);
+
+        final int pos = i;
+        imgv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder dialog = new AlertDialog.Builder(context);
+                View dialogView = View.inflate(context, R.layout.dialog1, null);
+                ImageView imgLarge = dialogView.findViewById(R.id.imgv_large);
+                imgLarge.setImageResource(posterIds[pos]);
+                dialog.setTitle("Large Poster");
+                dialog.setIcon(R.drawable.p1);
+                dialog.setView(dialogView);
+                dialog.setNegativeButton("close", null);
+                dialog.show();
+            }
+        });
+
         return imgv;
     }
 }
--- app/src/main/java/kr/hs/emirim/ko502804/project11_1/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/ko502804/project11_1/MainActivity.java
@@ -15,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
         grid1 = findViewById(R.id.grid1);
         GridAdapter adapter = new GridAdapter(this);
         grid1.setAdapter(adapter);
+
+
     }
 
 }
